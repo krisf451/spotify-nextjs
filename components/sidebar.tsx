d904643fb846cbe490b4c04bb63b89cb3b480b94
Kristian Fulkerson <kristianf451@gmail.com>
2022-05-20 15:02:17 -0700

    feature: added custom styling the to sidebar playlist section for scrolling, added in dummy data where our eventual playlist data from the backend will go

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -30,11 +30,24 @@ const navMenu = [
     route: "/search",
   },
   {
-    name: "Library",
+    name: "Your Library",
     icon: MdLibraryMusic,
     route: "/library",
   },
 ];
+const musicMenu = [
+  {
+    name: "Create Playlist",
+    icon: MdPlaylistAdd,
+    route: "/",
+  },
+  {
+    name: "Liked Songs",
+    icon: MdFavorite,
+    route: "/favorites",
+  },
+];
+const playlist = new Array(30).fill(1).map((_, i) => `Playlist Item ${i + 1}`);
 
 const Sidebar = () => {
   return (
@@ -45,7 +58,7 @@ const Sidebar = () => {
       paddingX="5px"
       color="gray"
     >
-      <Box paddingY="20px">
+      <Box paddingY="20px" height="100%">
         <Box width="120px" marginBottom="20px" paddingX="20px">
           <NextImage src="/logo.svg" height={60} width={120} />
         </Box>
@@ -63,6 +76,56 @@ const Sidebar = () => {
                       />
                       {item.name}
                     </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Box>
+          <List spacing={2}>
+            {musicMenu.map((item) => (
+              <ListItem paddingX="20px" fontSize="16px" key={item.name}>
+                <LinkBox>
+                  <NextLink href={item.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={item.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {item.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Divider marginY="10px" color="gray.800" />
+        <Box
+          height="66%"
+          overflowY="auto"
+          paddingY="20px"
+          css={{
+            "&::-webkit-scrollbar": {
+              width: "4px",
+            },
+            "&::-webkit-scrollbar-track": {
+              width: "6px",
+            },
+            "&::-webkit-scrollbar-thumb": {
+              background: "lightgray",
+              borderRadius: "24px",
+            },
+          }}
+        >
+          <List spacing={2}>
+            {playlist.map((item) => (
+              <ListItem paddingX="20px" key={item}>
+                <LinkBox>
+                  <NextLink href="/" passHref>
+                    <LinkOverlay>{item}</LinkOverlay>
                   </NextLink>
                 </LinkBox>
               </ListItem>
