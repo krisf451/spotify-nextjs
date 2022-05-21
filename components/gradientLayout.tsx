c89384270cb01f43e737bbe320fe7945648c6a5c
Kristian Fulkerson <kristianf451@gmail.com>
2022-05-21 15:17:03 -0700

    feat: finished the gradientLayout component and am testing it, UI looks like design

--- components/gradientLayout.tsx
+++ components/gradientLayout.tsx
@@ -16,7 +16,25 @@ const GradientLayout = ({
       bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.95) 75%)`}
     >
       <Flex bg={`${color}.600`} padding="40px" align="end">
-        hello
+        <Box padding="20px">
+          <Image
+            boxSize="160px"
+            boxShadow="2xl"
+            src={image}
+            borderRadius={roundImage ? "100%" : "3px"}
+          ></Image>
+        </Box>
+        <Box padding="20px" lineHeight="40px" color="white">
+          <Text fontSize="sm" fontWeight="bold" casing="uppercase">
+            {subtitle}
+          </Text>
+          <Text fontSize="6xl" fontWeight="bold">
+            {title}
+          </Text>
+          <Text fontSize="sm" fontWeight="300">
+            {description}
+          </Text>
+        </Box>
       </Flex>
     </Box>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,14 @@ import GradientLayout from "../components/gradientLayout";
 
 export default function Home() {
   return (
-    <GradientLayout color="blue">
+    <GradientLayout
+      color="blue"
+      title="Kristian Fulkerson"
+      subtitle="profile"
+      description="a sample description of the user"
+      roundImage
+      image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2"
+    >
       <div>Home</div>
     </GradientLayout>
   );
