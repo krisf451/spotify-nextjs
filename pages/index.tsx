3656be30b2d77c0e64c63d87d033c69d93f694e7
Kristian Fulkerson <kristianf451@gmail.com>
2022-05-21 16:08:18 -0700

    feat: updated seed file to inclue first and lastname for the user so we can render that to our home component instead of hard coded data, also pulling the number of playlists that user has by just adding that to what our useMe hook returns

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
+import { Box, Flex, Text, Image } from "@chakra-ui/react";
 import GradientLayout from "../components/gradientLayout";
 import prisma from "../lib/prisma";
-import { Box, Flex, Text, Image } from "@chakra-ui/react";
+import { useMe } from "../lib/hooks";
 
 const Home = ({ artists }) => {
+  const { user } = useMe();
+
   return (
     <GradientLayout
       color="purple"
-      title="Kristian Fulkerson"
+      title={`${user?.firstName} ${user?.lastName}`} //TODO: How do we want to handle loading state?
       subtitle="profile"
-      description="a sample description of the user"
+      description={`${user.playListsCount} public playlists`}
       roundImage
       image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2"
     >
@@ -21,7 +24,7 @@ const Home = ({ artists }) => {
         </Box>
         <Flex>
           {artists.map((artist) => (
-            <Box paddingX="10px" width="20%">
+            <Box paddingX="10px" width="20%" key={artist.id}>
               <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
                 <Image
                   src="http://placekitten.com/300/300"
