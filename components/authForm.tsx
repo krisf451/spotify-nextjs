9203a87a986bcdc90c492ebb9867f25544a20cb8
Kristian Fulkerson <kristianf451@gmail.com>
2022-05-20 20:02:23 -0700

    feat: added a form for signing in and signing up, alse added a button to switch between modes

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -3,20 +3,72 @@ import { useRouter } from "next/router";
 import { useSWRConfig } from "swr";
 import { auth } from "../lib/mutations";
 import { FC, useState } from "react";
+import NextImage from "next/image";
 
 const AuthForm: FC<{ mode: string }> = ({ mode }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
   const router = useRouter();
+  const [isSignup, setIsSignup] = useState(router.pathname === "/signup");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleSwitch = () => {
+    setIsSignup((prev) => !prev);
+    if (router.pathname === "/signin") {
+      router.push("/signup");
+    } else {
+      router.push("/signin");
+    }
+  };
 
   return (
     <Box height="100vh" width="100vw" bg="black" color="white">
-      <Flex justify="center" align="center" height="100px">
-        hello
+      <Flex
+        justify="center"
+        align="center"
+        height="100px"
+        borderBottom="white 1px solid"
+      >
+        <NextImage src="/logo.svg" height={60} width={120} />
       </Flex>
-      <Flex justify="center" align="center" height="calc(100vh-100px)">
-        form
+      <Flex justify="center" align="center" height="calc(100vh - 100px)">
+        <Box padding="50px" bg="gray.900" borderRadius="6px">
+          <form onSubmit={handleSubmit}>
+            <Input
+              placeholder="email"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              marginBottom="20px"
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              type="submit"
+              bg="green.500"
+              marginTop="20px"
+              isLoading={isLoading}
+              sx={{
+                "&:hover": {
+                  bg: "green.300",
+                },
+              }}
+            >
+              {mode}
+            </Button>
+          </form>
+          <Box onClick={handleSwitch} marginTop="20px" cursor="pointer">
+            {isSignup
+              ? "Already have an account? Sign In"
+              : "Dont have an account? Sign up"}
+          </Box>
+        </Box>
       </Flex>
     </Box>
   );
