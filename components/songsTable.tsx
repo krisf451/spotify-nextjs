b5081b225f4b321d147e603c70d8226e51250030
Kristian Fulkerson <kristianf451@gmail.com>
2022-05-21 19:01:55 -0700

    feat: added our table to the songsTable component and are rendering out all of a playlists songs, added a default image to make styling easier later

--- components/songsTable.tsx
+++ components/songsTable.tsx
@@ -8,14 +8,17 @@ import {
   Th,
   Tbody,
   IconButton,
-  Icon,
+  Image,
 } from "@chakra-ui/react";
 import { BsFillPlayFill, BsThreeDots } from "react-icons/bs";
 import { AiOutlineClockCircle, AiFillHeart } from "react-icons/ai";
 
+const defaultImage =
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F2.bp.blogspot.com%2F-YpFHvvLWH7c%2FUcQFriyLFjI%2FAAAAAAAACgw%2FSNi-PZdRy04%2Fs1600%2FUsher.jpg&f=1&nofb=1";
+
 const SongTable = ({ songs }) => {
   return (
-    <Box bg="transparent">
+    <Box bg="transparent" color="white">
       <Flex padding="10px" marginBottom="20px" align="center">
         <IconButton
           icon={<BsFillPlayFill fontSize="30px" />}
@@ -31,7 +34,48 @@ const SongTable = ({ songs }) => {
           style={{ marginLeft: "20px" }}
         />
       </Flex>
-      <Table></Table>
+      <Table variant="unstyled">
+        <Thead borderBottom="1px solid rgba(255,255,255,0.2)">
+          <Tr>
+            <Th>#</Th>
+            <Th>Title</Th>
+            <Th>Date Added</Th>
+            <Th>
+              <AiOutlineClockCircle />
+            </Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {songs?.map((song, i) => (
+            <Tr
+              sx={{
+                transition: ".3s all",
+                "&:hover": {
+                  bg: "rgba(255,255,255,.1)",
+                },
+              }}
+              key={song.id}
+              cursor="pointer"
+            >
+              <Td>{i}</Td>
+              <Td>
+                <Image
+                  src={defaultImage}
+                  boxSize="40px"
+                  objectFit="cover"
+                  alt="default"
+                  borderRadius="3px"
+                  marginRight="10px"
+                  display="inline-block"
+                />
+                {song.name}
+              </Td>
+              <Td>{song.createdAt.toString()}</Td>
+              <Td>{song.duration}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
     </Box>
   );
 };
